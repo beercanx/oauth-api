cac9455d122f683df75e1345ec4fa2f557c5b1d3
James Bacon <james@baconi.co.uk>
2021-03-28 13:31:17 +0100

    Added test for ClientSecret around not including secrets in its toString

---
+++ src/test/kotlin/com/sbgcore/oauth/api/authentication/ClientSecretTest.kt
@@ -0,0 +1,15 @@
+package com.sbgcore.oauth.api.authentication
+
+import com.sbgcore.oauth.api.client.ClientId.ConsumerZ
+import io.kotest.matchers.string.shouldNotContain
+import org.junit.jupiter.api.Test
+import java.util.*
+
+class ClientSecretTest {
+
+    @Test
+    fun `should not include secret in toString() output`() {
+        ClientSecret(UUID.randomUUID(), ConsumerZ, "password").toString() shouldNotContain "password"
+    }
+
+}
