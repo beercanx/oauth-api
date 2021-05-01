aea380de8670ea7282ee3823ba945b5f64a15651
James Bacon <james@baconi.co.uk>
2021-05-01 22:59:53 +0100

    Making secrets and credentials more secure.

--- src/main/kotlin/uk/co/baconi/oauth/api/client/ClientSecret.kt
+++ src/main/kotlin/uk/co/baconi/oauth/api/client/ClientSecret.kt
@@ -1,16 +1,10 @@
-@file:UseSerializers(UUIDSerializer::class)
-
 package uk.co.baconi.oauth.api.client
 
-import uk.co.baconi.oauth.api.serializers.UUIDSerializer
-import kotlinx.serialization.Serializable
-import kotlinx.serialization.UseSerializers
 import org.dizitart.no2.objects.Id
 import java.util.*
 
-@Serializable
 data class ClientSecret(@Id val id: UUID, val clientId: ClientId, val secret: String) {
     override fun toString(): String {
-        return "ClientSecret(id=$id, clientId=$clientId)"
+        return "ClientSecret(id=$id, clientId=$clientId, secret='REDACTED')"
     }
 }
