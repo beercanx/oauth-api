35b87756184c11ca8b771b8655dddb67a7a62f30
James Bacon <james@baconi.co.uk>
2022-10-08 15:39:40 +0100

    Minor code style changes
    private where it can be

--- src/main/kotlin/uk/co/baconi/oauth/api/CloseAndLog.kt
+++ src/main/kotlin/uk/co/baconi/oauth/api/CloseAndLog.kt
@@ -3,6 +3,9 @@ package uk.co.baconi.oauth.api
 import io.ktor.application.*
 import java.io.Closeable
 
+/**
+ * Close the [Closeable] and catch any exceptions and log them out.
+ */
 fun Application.closeAndLog(closeable: Closeable) {
     try {
         log.info("Closing ${closeable::class}")
