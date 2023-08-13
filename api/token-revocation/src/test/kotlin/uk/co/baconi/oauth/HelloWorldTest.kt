c5b0333412352172d17650d59e830314abf26098
James Bacon <james@baconi.co.uk>
2023-08-13 12:12:18 +0100

    Setting more modules up with test support and therefore more code coverage reports.

---
+++ api/token-revocation/src/test/kotlin/uk/co/baconi/oauth/HelloWorldTest.kt
@@ -0,0 +1,13 @@
+package uk.co.baconi.oauth
+
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+class HelloWorldTest {
+
+    @Test
+    fun hello() {
+        "hello" shouldBe "hello"
+    }
+
+}
