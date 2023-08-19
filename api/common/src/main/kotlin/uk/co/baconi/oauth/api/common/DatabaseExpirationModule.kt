ea6a14066a1342b1168e05a664f4b707e19d0fbd
James Bacon <james@baconi.co.uk>
2023-08-19 11:24:41 +0100

    Added some integration tests around providing database connections.

--- api/common/src/main/kotlin/uk/co/baconi/oauth/api/common/DatabaseExpirationModule.kt
+++ api/common/src/main/kotlin/uk/co/baconi/oauth/api/common/DatabaseExpirationModule.kt
@@ -1,31 +1,34 @@
 package uk.co.baconi.oauth.api.common
 
-import io.ktor.server.application.*
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import uk.co.baconi.oauth.api.common.authorisation.AuthorisationCodeRepository
-import uk.co.baconi.oauth.api.common.token.AccessTokenRepository
-
-interface DatabaseExpirationModule {
-
-    val accessTokenRepository: AccessTokenRepository
-    val authorisationCodeRepository: AuthorisationCodeRepository
-
-    fun Application.databaseExpiration() {
-
-        log.info("Registering the DatabaseModule.databaseExpiration() module")
-
-        // Every second, look for and remove any expired tokens or codes - TODO - Extract delay into config.
-        doIndefinitely(1_000, accessTokenRepository::deleteExpired)
-        doIndefinitely(1_000, authorisationCodeRepository::deleteExpired)
-    }
-
-    private fun Application.doIndefinitely(timeMillis: Long, block: suspend () -> Unit) {
-        launch {
-            while (true) {
-                block()
-                delay(timeMillis)
-            }
-        }
-    }
-}
+// TODO - Decide if we want to fix this problem with code or by choosing a database technology that supports it.
+//import io.ktor.server.application.*
+//import kotlinx.coroutines.Job
+//import kotlinx.coroutines.delay
+//import kotlinx.coroutines.launch
+//import uk.co.baconi.oauth.api.common.authorisation.AuthorisationCodeRepository
+//import uk.co.baconi.oauth.api.common.token.AccessTokenRepository
+//import uk.co.baconi.oauth.api.common.token.RefreshTokenRepository
+//
+//interface DatabaseExpirationModule {
+//
+//    val accessTokenRepository: AccessTokenRepository
+//    val refreshTokenRepository: RefreshTokenRepository
+//    val authorisationCodeRepository: AuthorisationCodeRepository
+//
+//    fun Application.databaseExpiration() {
+//
+//        log.info("Registering the DatabaseModule.databaseExpiration() module")
+//
+//        // Every second, look for and remove any expired tokens or codes - TODO - Extract delay into config.
+//        val accessTokenJob = doIndefinitely(1_000, accessTokenRepository::deleteExpired)
+//        val refreshTokenJob = doIndefinitely(1_000, refreshTokenRepository::deleteExpired)
+//        val authorisationCodeJob = doIndefinitely(1_000, authorisationCodeRepository::deleteExpired)
+//    }
+//
+//    private fun Application.doIndefinitely(timeMillis: Long, block: suspend () -> Unit): Job = launch {
+//        while (true) {
+//            block()
+//            delay(timeMillis)
+//        }
+//    }
+//}
