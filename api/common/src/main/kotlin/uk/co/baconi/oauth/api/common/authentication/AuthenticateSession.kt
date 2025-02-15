55fb333d9e629f60154d848ce18f93bca527d680
James Bacon <james@baconi.co.uk>
2025-02-15 14:22:53 +0000

    Upgraded Ktor from 2.3.11 to 3.1.0
    Upgraded Kotlin from 2.0.0 to 2.1.10
    
    Fixed incompatibilites with how the embedded server was created.
    
    Fixed serialisation of AuthenticateSession post Ktor upgrade.

--- api/common/src/main/kotlin/uk/co/baconi/oauth/api/common/authentication/AuthenticateSession.kt
+++ api/common/src/main/kotlin/uk/co/baconi/oauth/api/common/authentication/AuthenticateSession.kt
@@ -1,5 +1,11 @@
+@file:UseSerializers(UUIDSerializer::class)
+
 package uk.co.baconi.oauth.api.common.authentication
 
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.UseSerializers
+import uk.co.baconi.oauth.api.common.uuid.UUIDSerializer
 import java.util.*
 
+@Serializable
 data class AuthenticateSession(val csrfToken: UUID)
