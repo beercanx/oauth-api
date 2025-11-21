d2ddf72cc0f8335aabef6274d8159ec82c4758a4
James Bacon <james@baconi.co.uk>
2025-11-21 11:24:35 +0000

    Migrated test consumer compose to a version catalog
    This should improve dependabot integration.

--- test-consumers/compose/build.gradle.kts
+++ test-consumers/compose/build.gradle.kts
@@ -1,16 +1,14 @@
-import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
 
 plugins {
     // this is necessary to avoid the plugins to be loaded multiple times in each subproject's classloader
-    kotlin("jvm") apply false
-    kotlin("android") apply false
-    kotlin("multiplatform") apply false
-    kotlin("plugin.compose") apply false
+    alias(libs.plugins.kotlin.multiplatform) apply false
+    alias(libs.plugins.kotlin.plugin.compose) apply false
+    alias(libs.plugins.kotlin.plugin.serialization) apply false
 
-    id("com.android.library") apply false
-    id("com.android.application") apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.android.application) apply false
 
-    id("org.jetbrains.compose") apply false
+    alias(libs.plugins.jetbrains.compose) apply false
 }
 
 group = "uk.co.baconi.oauth.consumers.compose"
@@ -22,10 +20,4 @@ allprojects {
         mavenCentral()
     }
 
-    plugins.withId("org.jetbrains.kotlin.jvm") {
-        // Replacement for kotlin { jvmToolchain(21) } in each project
-        extensions.configure<KotlinJvmProjectExtension> {
-            jvmToolchain(21)
-        }
-    }
 }
