56be4fad7e265f80bc5068ffece6cc51596fd0b0
James Bacon <james@baconi.co.uk>
2026-05-02 12:15:40 +0100

    Bumping com.codeborne:selenide from 7.14.0 to 7.16.0
    Security patching selenide's org.apache.httpcomponents.client5:httpclient5 from 5.6 to 5.6.1

--- automation/browser/build.gradle.kts
+++ automation/browser/build.gradle.kts
@@ -20,6 +20,18 @@ dependencies {
     testImplementation(enforcedPlatform(libs.junit.bom))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    // Security patching
+    constraints {
+        implementation("org.apache.httpcomponents.client5:httpclient5:5.6.1") {
+            // Needs Selenide to upgrade to its Apache Http Client 5.6 to 5.6.1 or greater
+            because("""
+                |com.codeborne:selenide 7.16.0 brings in
+                |com.codeborne:selenide-core 7.16.0 that brings in
+                |org.apache.httpcomponents.client5:httpclient5 5.6
+            """.trimMargin())
+        }
+    }
 }
 
 tasks.withType<Test>().configureEach {
